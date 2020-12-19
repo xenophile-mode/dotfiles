4dbabaa9ffea7548ae5c4dd2c142626bb18b8b8c
xenophile-mode <erickgpersonalacct1@gmail.com>
2020-12-19 14:05:32 -0600

    updated dotfile update script

--- df.sh
+++ df.sh
@@ -1,33 +1,33 @@
 #!/bin/bash/
 #A bash script to automatically update dotfiles repo
 
-cp ~/.bashrc ~/archscript/dotfiles/bash
-cp ~/.bash_aliases ~/archscript/dotfiles/bash
+cp ~/.bashrc ~/dotfiles/bash
+cp ~/.bash_aliases ~/dotfiles/bash
 
-cp ~/.config/dmenu-extended/config/dmenuExtended_preferences.txt ~/archscript/dotfiles/dmenu_extended
+cp ~/.config/dmenu-extended/config/dmenuExtended_preferences.txt ~/dotfiles/dmenu_extended
 
-cp ~/.local/src/dwm/config.h ~/archscript/dotfiles/dwm
+cp ~/.local/src/dwm/config.h ~/dotfiles/dwm
 
-sudo cp /usr/share/dwmbar/config ~/archscript/dotfiles/dwmbar
+sudo cp /usr/share/dwmbar/config ~/dotfiles/dwmbar
 
-cp ~/.config/fish/functions/fish_import_bash_aliases.fish ~/archscript/dotfiles/fish
+cp ~/.config/fish/functions/fish_import_bash_aliases.fish ~/dotfiles/fish
 
-sudo cp /etc/default/grub ~/archscript/dotfiles/grub
-sudo cp /usr/sbin/update-grub ~/archscript/dotfiles/grub
+sudo cp /etc/default/grub ~/dotfiles/grub
+sudo cp /usr/sbin/update-grub ~/dotfiles/grub
 
-cp ~/.config/qutebrowser/autoconfig.yml ~/archscript/dotfiles/qutebrowser
+cp ~/.config/qutebrowser/autoconfig.yml ~/dotfiles/qutebrowser
 
-cp ~/.config/termite ~/archscript/dotfiles/terms/termite
+cp ~/.config/termite ~/dotfiles/terms/termite
 
-cp ~/.vimrc ~/archscript/dotfiles
+cp ~/.vimrc ~/dotfiles
 
-cp ~/.xprofile ~/archscript/dotfiles
+cp ~/.xprofile ~/dotfiles
 
-cp /usr/share/xsessions/dwm.desktop ~/archscript/dotfiles
+cp /usr/share/xsessions/dwm.desktop ~/dotfiles
 
-cp ~/.config/picom.conf ~/archscript/dotfiles 
+cp ~/.config/picom.conf ~/dotfiles 
 
-cd ~/archscript
+cd ~/dotfiles
 git add .
 git commit -m "updated dotfiles"
 git push
